91efc91ff78dc722fd8837bda7053d4a24c975d9
Leepermatt <131700890+Leepermatt@users.noreply.github.com>
2024-11-02 19:05:15 -0700

    week7.4

--- Mileage07/obj/Debug/net8.0-windows/Milage tracker.AssemblyInfo.cs
+++ Mileage07/obj/Debug/net8.0-windows/Milage tracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Milage tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c0cfd1bd915615b453a0210f3a04d4a68e08d6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce5861d5d6e679925767f6906b27e1a3c4fdd3a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Milage tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Milage tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
